62f7f12055d00f9d6883414e4dae1c389f54b48c
Prasetyo <prasetyosyifa@gmail.com>
2023-09-14 16:55:50 +0700

    handle cors

--- index.js
+++ index.js
@@ -1,19 +1,21 @@
 const express = require("express");
+const cors = require("cors");
 const bodyParser = require("body-parser");
 const router = require("./routes");
 const db = require("./config/database");
-const swaggerUi = require('swagger-ui-express');
-const swaggerDocs = require('./docs/swagger.json');
+const swaggerUi = require("swagger-ui-express");
+const swaggerDocs = require("./docs/swagger.json");
 
 const port = 8080;
 
-db.on('error', (error) => console.error(error))
-db.once('open', () => console.log('Connected to Database'))
+db.on("error", (error) => console.error(error));
+db.once("open", () => console.log("Connected to Database"));
 
 const app = express();
+app.use(cors());
 app.use(bodyParser.json());
 app.use(router);
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
 app.listen(port, () => {
   console.log(`App listen on port ${port}`);
